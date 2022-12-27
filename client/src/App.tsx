6e0aa9928af59dc3048f5f103028c030ab0c4dfb
Alex Gushulak <alex.gushulak@gmail.com>
2022-12-27 09:17:54 -0600

    Updated photos

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,16 @@
 import MediaCard from './components/MediaCard';
 import MainNavBar from './components/MainNavBar';
+import imgUrlBusiness from './assets/business.jpeg';
+import imgUrlCliff from './assets/cliff.jpg';
+import imgUrlComputer from './assets/computer.jpg';
 
 export default function App() {
   return (
     <div>
       <MainNavBar />
-      <MediaCard title={"Portfolio Project"} description={"Desc 1"} imagePath= {("https://images.unsplash.com/photo-1587831990711-23ca6441447b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZGVza3RvcCUyMGNvbXB1dGVyfGVufDB8fDB8fA%3D%3D&w=1000&q=80")} />
-      <MediaCard title={"Machine Learning Project"} description={"Desc 2"} imagePath={"./src/assets/business.jpeg"} />
-      <MediaCard title={"Thermal Tracker GUI"} description={"Desc 3"} imagePath={"./src/assets/business.jpeg"}/>
+      <MediaCard title={"Portfolio Project"} description={"Desc 1"} imagePath= {(imgUrlComputer)} />
+      <MediaCard title={"Machine Learning Project"} description={"Desc 2"} imagePath={imgUrlBusiness} />
+      <MediaCard title={"Thermal Tracker GUI"} description={"Desc 3"} imagePath={imgUrlCliff}/>
     </div>
   );
 }
