38a2ad1c68890560e82cab6c6bf43dffeb04473a
Alex Gushulak <alex.gushulak@gmail.com>
2022-12-27 23:02:38 -0600

    Fading style

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,15 @@
 import Container from '@mui/material/Container';
 import MediaCard from './components/MediaCard';
 import MainNavBar from './components/MainNavBar';
+import JobList from './components/JobList';
 import IntroductionCard from './components/IntroductionCard';
 import imgUrlBusiness from './assets/business.jpeg';
 import imgUrlCliff from './assets/cliff.jpg';
 import imgUrlComputer from './assets/computer.jpg';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-
+import Fade from '@mui/material/Fade';
+import Slide from '@mui/material/Slide';
 
 const darkTheme = createTheme({
   palette: {
@@ -25,16 +27,23 @@ const darkTheme = createTheme({
 export default function App() {
   return (
     <div>
-      <ThemeProvider theme={darkTheme}>
-        <CssBaseline />
-      <MainNavBar />
-      <IntroductionCard />
-      <Container>
-        <MediaCard title={"Portfolio Project"} description={"Desc 1"} imagePath= {(imgUrlComputer)} />
-        <MediaCard title={"Machine Learning Project"} description={"Desc 2"} imagePath={imgUrlBusiness} />
-        <MediaCard title={"Thermal Tracker GUI"} description={"Desc 3"} imagePath={imgUrlCliff}/>
-      </Container>
-      </ThemeProvider>
+        <ThemeProvider theme={darkTheme}>
+          <CssBaseline />
+          <MainNavBar />
+          <Fade in={true} timeout={500}>
+            <div>
+              <IntroductionCard />
+          <Container sx={{mb:10}}>
+            <JobList />
+          </Container>
+          <Container sx={{mb:10}}>
+            <MediaCard title={"Portfolio Project"} description={"Desc 1"} imagePath= {(imgUrlComputer)} />
+            <MediaCard title={"Machine Learning Project"} description={"Desc 2"} imagePath={imgUrlBusiness} />
+            <MediaCard title={"Thermal Tracker GUI"} description={"Desc 3"} imagePath={imgUrlCliff}/>
+          </Container>
+          </div>
+          </Fade>
+        </ThemeProvider>
     </div>
   );
 }
