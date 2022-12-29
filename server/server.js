c23647f468d9bc13771a5765309bd435a988996c
Alex Gushulak <alex.gushulak@gmail.com>
2022-12-28 21:51:43 -0600

    Testing server for deploy testing

--- server/server.js
+++ server/server.js
@@ -1,9 +1,19 @@
 const express = require('express')
+const res = require('express/lib/response')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.get('/info', (req, res) => {
+    console.log("Some is entering the info route")
+    res.status(200).send({
+        name: "John Doe",
+        age: 30,
+        address: "1234 Main St"
+    })
 })
 
 app.listen(port, ()=>{
