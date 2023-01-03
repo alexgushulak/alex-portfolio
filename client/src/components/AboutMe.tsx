906166e42f6e4a95ed53be40f3d21648a2e224cc
Alex Gushulak <alex.gushulak@gmail.com>
2023-01-02 19:32:14 -0600

    Remove the letter e

--- client/src/components/AboutMe.tsx
+++ client/src/components/AboutMe.tsx
@@ -9,7 +9,6 @@ export default function AboutMe() {
             maxWidth="md"
             sx={{'margin-top': {xs: 25, sm: 25}, 'margin-bottom': {xs: 75, sm: 100}}}
         >
-                e
             <Typography variant="body1" align="left" component="div" sx={{'max-width':700, textAlign:'left'}} gutterBottom>
             Hi there, my name is Alex Gushulak and I am a software engineer with a background in mechanical engineering. 
             I received my degree from Northeastern University and have since focused on full stack web development, 
