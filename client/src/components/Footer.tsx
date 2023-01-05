3f03fd0f8b3c75709bf640226878b596a383c880
Alex Gushulak <alex.gushulak@gmail.com>
2023-01-05 16:11:30 -0600

    Footer changes

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -13,18 +13,18 @@ export default function Footer() {
         <div>
         <Divider variant="fullWidth" />
         <Box sx={{ display: 'flex', 'width': '90%', 'margin': '0px auto ', 'padding':'10px', height: '75px' }}>
-            <Typography variant="body2" sx={{ color: "#fff", m: 1.5, 'font-size': '14px', width: '150px', 'padding-top': '2px'}}>Built in Boston.</Typography>
+            <Typography variant="body2" sx={{ color: "#fff", m: 1, 'font-size': '14px', width: '150px', 'padding-top': '2px'}}>Built in Boston.</Typography>
             <Stack direction="row"
                 divider={<Divider orientation="vertical" flexItem />}
                 spacing={1} sx={{mt: 0.4, 'float': 'right!important', 'display': 'flex', 'width': '100%', 'justify-content':'right'}}>
                 <Button sx={{display: 'inline-block'}} href="https://www.linkedin.com/in/alex-g-33039513b/">
-                <LinkedInIcon  sx={{ color: "#fff", fontSize: 20, m: 1 }} />
+                <LinkedInIcon  sx={{ color: "#fff", fontSize: 18, m: 1 }} />
                 </Button>
                 <Button sx={{display: 'inline-block'}} href="https://github.com/alexgushulak">
-                <GitHubIcon sx={{ color: "#fff", fontSize: 20, m: 1 }} />
+                <GitHubIcon sx={{ color: "#fff", fontSize: 18, m: 1 }} />
                 </Button>   
                 <Button sx={{display: 'inline-block'}} href="mailto: alex.gushulak@gmail.com">
-                <EmailIcon sx={{ color: "#fff", fontSize: 20, m: 1 }} />
+                <EmailIcon sx={{ color: "#fff", fontSize: 18, m: 1 }} />
                 </Button>
             </Stack>
         </Box>
